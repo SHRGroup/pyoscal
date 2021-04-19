003c5c21366a8e6312528740202c9bb4f06179bf
Matthew Ruge <matthew.ruge@shrgroupllc.com>
2021-04-19 10:42:26 -0400

    added git username and email to allow commits

--- update_oscal.sh
+++ update_oscal.sh
@@ -1,8 +1,12 @@
+git config --global user.email "${GITLAB_USER_EMAIL}"
+git config --global user.name "${GITLAB_USER_ID}"
+
 pushd OSCAL
 git fetch --tags
 latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git checkout $latestTag
 popd 
+
 git add OSCAL 
 git commit -m 'Updated to latest NIST OSCAL TAG'
 # git push
